432062c1386756a084b9479ff708369d3f363830
rlaehdals <rkdlem48@gmail.com>
2021-09-14 14:14:21 +0900

    Initial commit

---
+++ src/main/java/ex/jenkins/JenkinsApplication.java
@@ -0,0 +1,13 @@
+package ex.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ex/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
