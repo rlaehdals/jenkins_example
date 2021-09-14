7200a0c5c843548b97cbc61d22a79da22a711747
rlaehdals <rkdlem48@gmail.com>
2021-09-14 15:51:40 +0900

    .

--- src/main/java/ex/jenkins/HelloController.java
+++ src/main/java/ex/jenkins/HelloController.java
@@ -1,13 +1,31 @@
 package ex.jenkins;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
 
+    private final Environment env;
     @GetMapping("/")
     public String hello(){
         return "ok2";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
